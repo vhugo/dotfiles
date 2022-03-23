8fee19fba239b0c72e516041a0b7e2fef8032a53
Victor Alves <victor@sub.pro.br>
2022-03-23 19:20:33 +0100

    Update zsh config for tracker

--- zsh/config/tracker.zsh
+++ zsh/config/tracker.zsh
@@ -4,6 +4,14 @@ install-tkr(){
   mkdir -p $TKR_HOME/devlog $TKR_HOME/planning
 }
 
+tkr-list(){
+  tree $TKR_HOME
+}
+
+tkr-search(){
+  grep -i $@ $TKR_HOME/*/*.md 
+}
+
 tkr-devlog(){
   $EDITOR $TKR_HOME/devlog/devlog-$(date '+%Y%m%d').md
 }
@@ -12,6 +20,8 @@ tkr-okr(){
   $EDITOR $TKR_HOME/planning/okr.md
 }
   
+# todo: creates a daily file for todos. it copies the latest todo as a bootstrap
+# for the new one.
 tkr-todo(){
   local todo_file=todo-$(date '+%Y%m%d').md
 
@@ -36,14 +46,36 @@ tkr-meeting(){
 }
 
 tkr-git(){
-  git --git-dir $TKR_HOME/.git/ --work-tree $TKR_HOME $argv
+  git --git-dir $TKR_HOME/.git/ --work-tree $TKR_HOME $@
 }
 
-tkr-list(){
-  tree $TKR_HOME
+# gamedev: major gamedev under development.
+tkr-gamedev(){
+  $EDITOR $TKR_HOME/planning/gamedev-untitled-space-theme-cozy-game.md
 }
 
-tkr-search(){
-  grep -i $argv $TKR_HOME/*/*.md 
+# microgamedev: microgame under development.
+tkr-microgamedev(){
+  $EDITOR $TKR_HOME/planning/microgamedev-dot-detective.md
+}
+
+_gitcmdshare(){
+  git --git-dir $gamedev_share/.git/ --work-tree $gamedev_share $@
+}
+
+# gamedev-share: publish my major gamedev documents for sharing outside my tracker files.
+tkr-gamedev-share(){
+  local gamedev_share=~/local/project/gamedev-docs/
+
+  cp $TKR_HOME/planning/gamedev-* $gamedev_share  
+  mv -f $gamedev_share/gamedev-untitled-space-theme-cozy-game.md $gamedev_share/README.md
+  # _gitcmdshare status
+  _gitcmdshare add .
+  _gitcmdshare commit -m "auto-update gamedev docs on $(date '+%Y%m%d %H:%M')"
+  _gitcmdshare push
+}
+
+tkr-test(){
+  echo "auto-update gamedev docs on $(date '+%Y%m%d %H:%M')"
 }
 
