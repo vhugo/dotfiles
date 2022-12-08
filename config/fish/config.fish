2143c792179c93e17acecb5e746aee2a1f1c5fd5
Victor Alves <victor@sub.pro.br>
2022-12-08 12:29:29 +0100

    feat: change i3 workspace positions and icons
    Add more run for other terminals. I end up in need for more terminals.
    so now alacritty is no longer assigned to a workspace and 1-3 are spaces
    for terminal instances - may also use workspace for 4 if needed.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,3 +14,10 @@ set -gx SHELL /bin/fish
 set -gx TERMINAL alacritty
 set -g fish_key_bindings fish_vi_key_bindings
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/vhugo/installation/google-cloud-sdk/path.fish.inc' ]; . '/home/vhugo/installation/google-cloud-sdk/path.fish.inc'; end
+
+# pnpm
+set -gx PNPM_HOME "/home/vhugo/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
